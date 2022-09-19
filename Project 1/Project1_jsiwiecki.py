8a68df85c2a2baae8fc73fb629f20665d038b294
JRSiwiecki <JRSiwiecki@gmail.com>
2022-09-18 23:14:32 -0700

    Implement task 2

--- Project 1/Project1_jsiwiecki.py
+++ Project 1/Project1_jsiwiecki.py
@@ -3,6 +3,10 @@
 # Completed: 9/13/2022
 # Class: CS 2520.01
 
+from math import pi
+from math import factorial
+from math import sin
+
 # --------------------- TASK 1 ---------------------
 
 print("Welcome to Joseph's BMI Calculator!")
@@ -76,3 +80,54 @@ else:
     
     
 # --------------------- TASK 2 ---------------------
+
+# infinite loop, user exits when they please
+while True:
+    
+    # get input and evaluate it with the math library in case "pi" is entered
+    value = input("Enter an x value in radians (x must be in the range [-π, π]), or enter Q to quit: ")
+    
+    # sentinel value
+    if value == "Q" or value == "q":
+        break
+    
+    # separate value to print original input later
+    x = value
+    
+    # use try except in case the user enters an invalid input (not a number)
+    try:
+        x = eval(value)
+    
+    except: 
+        print("Your input is invalid!")
+    
+    # check if input is out of range
+    if x < -pi or x > pi:
+        print("Your input is out of range!")
+        continue
+    
+    # keep track of result and count (for number of loop iterations)
+    result = 0
+    count = 0
+    
+    for i in range(0, 150):
+        
+        # taylor series formula
+        result += ( ( -1 ) ** i ) / ( factorial ( ( 2 * i ) + 1 ) ) * ( x ** ( ( 2 * i ) + 1 ) )
+        
+        # increment count
+        count += 1
+        
+        # print intermediate result
+        print("Iteration", count, "=", result)
+        
+        # keep track of absolute difference between my calculation and python's sin function
+        difference = abs(result - sin(x))
+        
+        # if the difference between my result and python's result is low enough, stop calculations
+        if difference < 0.000001:
+            break
+        
+    # round result after calculation for more consistent comparison experience
+    result = round(result, 8)
+    print("When x =", value, "--> sin(x) using Taylor's formula is", result, "~", count, "loops")
