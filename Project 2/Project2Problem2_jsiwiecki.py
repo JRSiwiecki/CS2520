c989ecc44eb9e38df951896542100532993d5b39
JRSiwiecki <JRSiwiecki@gmail.com>
2022-10-10 18:34:22 -0700

    problem 2 initial

---
+++ Project 2/Project2Problem2_jsiwiecki.py
@@ -0,0 +1,7 @@
+# Author: Joseph Siwiecki
+# Assignment: Project 2
+# Completed: 10/10/2022
+# Class: CS 2520.01
+
+# --------------------- PROBLEM 1 ---------------------
+# ----------- PART 1 -----------
