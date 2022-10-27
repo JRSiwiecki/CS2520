27bfe0a9403ee6c0a6231d43b7dfa8c589aae311
Joseph <JRSiwiecki@gmail.com>
2022-10-27 11:35:36 -0700

    Initial lab 8 and project 3

---
+++ Lab 8/Lab8_jsiwiecki.py
@@ -0,0 +1,6 @@
+# Author: Joseph Siwiecki
+# Assignment: Lab 8
+# Completed: 10/27/2022
+# Class: CS 2520.01
+
+# --------------------- TASK 1 ---------------------
---
+++ Project 3/Project3_jsiwiecki.py
@@ -0,0 +1,7 @@
+# Author: Joseph Siwiecki
+# Assignment: Project 3
+# Completed: 10/27/2022
+# Class: CS 2520.01
+
+# --------------------- TASK 1 ---------------------
+# ----------- PART 1 -----------
